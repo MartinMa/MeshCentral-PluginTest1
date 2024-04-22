541f208680bfac27e5ca169f763ebf2888c098d6
Martin Mädler <martin.maedler@gmail.com>
2024-04-22 12:41:27 +0200

    Update

--- plugin_test1.js
+++ plugin_test1.js
@@ -3,7 +3,7 @@
 * @author Martin Mädler
 * @copyright 
 * @license Apache-2.0
-* @version v0.0.2
+* @version v0.0.3
 */
 
 "use strict";
@@ -26,6 +26,39 @@ module.exports.plugin_test1 = function (parent) {
                 var type = 4; // x64
                 var id = 'NQTcCpzMvtZ5Mnfi0HRYke2mNfHZcZF@B@w2POWZcW9yTFOXPkVDgk5SJclCaFSx';
                 u += 'id=' + type + '&meshid=' + id;
+                // For example:
+                // https://localhost/meshagents?id=4&meshid=NQTcCpzMvtZ5Mnfi0HRYke2mNfHZcZF@B@w2POWZcW9yTFOXPkVDgk5SJclCaFSx
+                // #########
+
+                const options = { rejectUnauthorized: false, checkServerIdentity: onVerifyServer }
+                const fs = require('fs');
+                const https = require('https');
+                var downloadSize = 0;
+                const req = https.request(u, options, function (res) {
+                    if (res.statusCode != 200) {
+                        console.log('Download error, statusCode: ' + res.statusCode);
+                        process.exit(1);
+                    } else {
+                        // Agent filename
+                        var agentFileName = 'supercool';
+                        // Check if this file already exists
+                        if (fs.existsSync(agentFileName)) { console.log('File \"' + agentFileName + '\" already exists.'); process.exit(1); }
+                        var fd = fs.openSync(agentFileName, 'w'); // Open the file for writing
+                        res.on('data', function (d) {
+                            downloadSize += d.length;
+                            fs.writeSync(fd, d); // Save to file
+                        });
+                        res.on('end', function (d) {
+                            fs.closeSync(fd); // Close file
+                            console.log('Downloaded ' + downloadSize + ' byte(s) to \"' + agentFileName + '\"');
+                            process.exit(1);
+                        });
+                    }
+                })
+                req.on('error', function (error) { console.error(error); process.exit(1); })
+                req.end();
+
+                // #########
                 res.set({ 'Content-Type': 'application/json' });
                 res.status(200);
                 res.send(JSON.stringify({
@@ -37,3 +70,5 @@ module.exports.plugin_test1 = function (parent) {
 
     return obj;
 }
+
+function onVerifyServer(clientName, certs) { return null; }
