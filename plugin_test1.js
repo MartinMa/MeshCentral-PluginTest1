7e0f2f218c9230552c45486dcf845663004c9a37
Martin Mädler <martin.maedler@gmail.com>
2024-04-29 10:05:46 +0200

    Update plugin

--- plugin_test1.js
+++ plugin_test1.js
@@ -3,7 +3,7 @@
 * @author Martin Mädler
 * @copyright 
 * @license Apache-2.0
-* @version v0.0.4
+* @version v0.0.5
 */
 
 "use strict";
@@ -20,52 +20,83 @@ module.exports.plugin_test1 = function (parent) {
             var url = domain.url;
             // Add custom endpoints here.
             obj.app.get(url + 'api/agentdownload', function (req, res) {
-                var url = 'wss://localhost/control.ashx';
-                var u = url.replace('wss://', 'https://').replace('/control.ashx', '/meshagents');
-                if (u.indexOf('?') > 0) { u += '&'; } else { u += '?'; }
-                var type = 4; // x64
-                var id = 'NQTcCpzMvtZ5Mnfi0HRYke2mNfHZcZF@B@w2POWZcW9yTFOXPkVDgk5SJclCaFSx';
-                u += 'id=' + type + '&meshid=' + id;
-                // For example:
-                // https://localhost/meshagents?id=4&meshid=NQTcCpzMvtZ5Mnfi0HRYke2mNfHZcZF@B@w2POWZcW9yTFOXPkVDgk5SJclCaFSx
-                // #########
+                // Example: api/agentdownload?id=4&meshid=NQTcCpzMvtZ5Mnfi0HRYke2mNfHZcZF@B@w2POWZcW9yTFOXPkVDgk5SJclCaFSx
+                // First, determine operating system, architecture and mesh id (device group)
+                const type = req.query.type;
+                const meshid = req.query.meshid;
+                if (type !== 4 && type !== 3) {
+                    // We only support Windows!
+                    sendBadRequest(res, 'api/agentdownload');
+                    return;
+                }
+                // Second, download meshagent to a temp directory
+                let downloadUrl = `https://localhost/meshagents/?id=${type}&meshid=${meshid}&installflags=2`
 
                 const options = { rejectUnauthorized: false, checkServerIdentity: onVerifyServer }
                 const fs = require('fs');
                 const https = require('https');
-                var downloadSize = 0;
-                const req1 = https.request(u, options, function (res) {
-                    if (res.statusCode != 200) {
-                        console.log('Download error, statusCode: ' + res.statusCode);
-                        process.exit(1);
+                const downloadReq = https.request(downloadUrl, options, function (downloadRes) {
+                    if (downloadRes.statusCode != 200) {
+                        sendInternalServerError(res, 'api/agentdownload');
                     } else {
                         // Agent filename
                         var agentFileName = 'supercool';
                         // Check if this file already exists
-                        if (fs.existsSync(agentFileName)) { console.log('File \"' + agentFileName + '\" already exists.'); process.exit(1); }
-                        var fd = fs.openSync(agentFileName, 'w'); // Open the file for writing
-                        res.on('data', function (d) {
-                            downloadSize += d.length;
-                            fs.writeSync(fd, d); // Save to file
+                        if (fs.existsSync(agentFileName)) {
+                            // File already exists
+                            sendInternalServerError(res, 'api/agentdownload');
+                        }
+                        // Open the file for writing
+                        let fileDescriptor = fs.openSync(agentFileName, 'w');
+                        downloadRes.on('data', function (chunck) {
+                            // Save to file
+                            fs.writeSync(fileDescriptor, chunck);
                         });
-                        res.on('end', function (d) {
-                            fs.closeSync(fd); // Close file
-                            console.log('Downloaded ' + downloadSize + ' byte(s) to \"' + agentFileName + '\"');
-                            process.exit(1);
+                        downloadRes.on('end', function () {
+                            // Close file
+                            fs.closeSync(fileDescriptor);
+                            // Third, call setup script
+                            const arch = type === 4 ? 'x64' : 'x86';
+                            // TODO
                         });
                     }
                 })
-                req1.on('error', function (error) { console.error(error); process.exit(1); })
-                req1.end();
+                downloadReq.on('error', (error) => {
+                    sendInternalServerError(res, 'api/agentdownload', error);
+                    return;
+                });
+                downloadReq.end();
 
-                // #########
+                // Prepare response
                 res.set({ 'Content-Type': 'application/json' });
                 res.status(200);
                 res.send(JSON.stringify({
-                    value: u
+                    value: downloadUrl
                 }));
             });
         }
+    }
+
+    function sendBadRequest(res, path, err) {
+        res.status(400);
+        sendJson(res, {
+            timestamp: new Date().toISOString(),
+            status: 400,
+            error: 'Bad Request',
+            message: err?.message,
+            path
+        });
+    }
+
+    function sendInternalServerError(res, path, err) {
+        res.status(500);
+        sendJson(res, {
+            timestamp: new Date().toISOString(),
+            status: 500,
+            error: 'Internal Server Error',
+            message: err?.message,
+            path
+        });
     }
 
     return obj;
